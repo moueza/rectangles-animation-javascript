b1cfb5486a95d06b70a19f3301cacf5725c44f7c
moueza <221372+moueza@users.noreply.github.com>
2021-04-20 09:59:49 +0200

    v13

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 (
 function(){
-console.log("in consol v10");
+console.log("in consol v13");
   // jQuery methods go here...
 document.getElementById("div1").innerHTML = "Hello JavaScript!";
+  
+  //https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API
+  const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+ctx.fillStyle = 'green';
+ctx.fillRect(10, 10, 150, 100);
 }
 )();
