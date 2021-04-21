b59c449ef3f27b9f0db5021db6d78e0a9ab76778
moueza <>
2021-04-21 09:47:43 +0200

    see #2 gen OK all 3/3 canvas

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
-(
-function(){
-  const vers=" v14";
-console.log("in consol"+vers);
-  // jQuery methods go here...
-document.getElementById("div1").innerHTML = "Hello JavaScript!";
+document.addEventListener('DOMContentLoaded',domloaded,false);
+function domloaded(){
+    const vers = " v14";
+    console.log("in consol+" + vers);
+    // jQuery methods go here...
+    document.getElementById("div1").innerHTML = "Hello JavaScript!";
   
-  //https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API
-  const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+    //https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API
+    //rectangle vert 
+    const canvas1 = document.getElementById("canvas");
+    const ctx1 = canvas1.getContext("2d");
+  
+    ctx1.fillStyle = "green";
+    ctx1.fillRect(10, 10, 150, 100);
+  
+  
+    //canevas 3 :
+    var c3 = document.getElementById("myCanvas3");
+    const c33 = document.getElementById("myCanvas3");
 
-ctx.fillStyle = 'green';
-ctx.fillRect(10, 10, 150, 100);
+    var ctx3 = c3.getContext("2d");
+    const ctx33= c33.getContext("2d");
+
+    ctx3.moveTo(0, 0);
+    ctx3.lineTo(200, 100);
+    ctx3.stroke();
 }
-)();
+
+
